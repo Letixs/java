5fe13a722fe72ec44852712838f3749650c73a46
Letixs <letqueirogaa@outlook.com>
2023-03-14 11:52:45 -0300

    Projeto Hello World

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+		int teste = 10;
+		
+		System.out.println("Hello World!\n");
+		System.out.println("Variável teste: " + teste);
+	}
+
+}
