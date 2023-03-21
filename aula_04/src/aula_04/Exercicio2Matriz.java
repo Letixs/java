23428fe22d85f63caf8d86635082eb1c2da477f0
Letixs <letqueirogaa@outlook.com>
2023-03-21 00:34:05 -0300

    Exercicio Matriz

--- aula_04/src/aula_04/Exercicio2Matriz.java
+++ aula_04/src/aula_04/Exercicio2Matriz.java
@@ -8,27 +8,34 @@ public class Exercicio2Matriz {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		float notas [][] = new float [10][4];
+		double notas [][] = {
+				{ 4.0, 5.0, 7.0, 3.0 },
+				{ 2.5, 6.5, 4.7, 8.0 },
+				{ 10.0, 8.5, 9.5, 8.0 },
+				{ 9.0, 6.5, 7.6, 8.2 },
+				{ 5.0, 5.0, 5.0, 6.3 },
+				{ 7.0, 8.0, 9.0, 8.5 },
+				{ 5.5, 3.5, 2.5, 1.0 },
+				{ 8.0, 9.0, 10.0, 9.5 },
+				{ 5.6, 5.8, 6.5, 7.0 },
+				{ 7.5, 8.5, 9.5, 10.0 }
+		};
 		float media = 0;
 		float medias [] = new float [10];
 		
-		for (int iLinha = 0; iLinha < notas.length; iLinha ++) {
-			
-			for (int iColuna = 0; iColuna < notas[iLinha].length; iColuna ++) {
-				System.out.println("Digite um número: ");
-				notas[iLinha][iColuna] = scanner.nextFloat();
-				
-				media += notas[iLinha][iColuna];
-				medias[iLinha] = media/4;	
-
-				
-				}
-			
-		
+		for (int i = 0; i < notas.length; i++) {
+			media = 0;
+			for (int c = 0; c < notas[i].length; c++) {
+				System.out.println("--------------------------");
+				System.out.println("Valor: " + notas[i][c]);
+				System.out.println("--------------------------");
+				media += notas[i][c];
+			}
+			medias[i] = media / notas[i].length;
 		}
 		
 		for(float i : medias) {
-			System.out.println(medias);
+			System.out.println(i);
 		}
 		
 		scanner.close();
